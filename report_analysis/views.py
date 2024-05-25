13f902f35e2bfd0d22263dc8d0f9bb667b5f0d03
Om Parghale <130880556+omArray99@users.noreply.github.com>
2024-05-26 02:42:06 +0530

    Update views.py

--- report_analysis/views.py
+++ report_analysis/views.py
@@ -6,7 +6,7 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.conf import settings
 from django.templatetags.static import static
-from .utils import explanation_pipeline, qa_system
+# from .utils import explanation_pipeline, qa_system
 import os
 import json
 import logging
@@ -18,10 +18,95 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.pdfgen.canvas import Canvas
 
+import cv2
+import numpy as np
+import torch
+import pytesseract
+from PIL import Image
+import re
+from typing import List
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def main(request):
     return render(request, 'report_analysis/main.html')
 
+model_name = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
+model = AutoModelForCausalLM.from_pretrained(model_name,device_map="auto",trust_remote_code=False,revision="main")
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+
+
+def preprocess_image(image):
+    np_image = np.array(image)
+    gray_image = cv2.cvtColor(np_image, cv2.COLOR_BGR2GRAY)
+    thresholded_img = cv2.adaptiveThreshold(
+        gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 275, 60)
+    return thresholded_img
+
+
+def extract_text(image):
+    return pytesseract.image_to_string(image)
+
+
+def preprocess_text(text):
+    text = text.lower()
+    allowed_chars = "abcdefghijklmnopqrstuvwxyz0123456789 ,.-()/\%+*=\n;"
+    text = ''.join(c for c in text if c in allowed_chars)
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r'\s*\n\s*', '\n', text)
+    return text
+
+
+def split_into_sentences(paragraph: str) -> List[str]:
+    sentence_endings = re.compile(
+        r"(?<=[.!?])\s+(?=[A-Z])", re.IGNORECASE)
+    sentences = sentence_endings.split(paragraph)
+    return sentences
+
+
+def group_sentences(sentences: List[str], group_size: int = 3) -> List[str]:
+    grouped_sentences = [' '.join(sentences[i:i + group_size])
+                         for i in range(0, len(sentences), group_size)]
+    return grouped_sentences
+
+
+def process_image(image_path):
+    image = Image.open(image_path)
+    processed_image = preprocess_image(image)
+    extracted_text = extract_text(processed_image)
+    preprocessed_text = preprocess_text(extracted_text)
+    sentences = split_into_sentences(preprocessed_text)
+    grouped = group_sentences(sentences)
+    return grouped
+
+
+def explanation_pipeline(image_path, output_str=""):
+    sequences = process_image(image_path)
+    logging.getLogger("transformers").setLevel(logging.ERROR)
+    for i, sent in enumerate(sequences):
+        if i == 0:
+            prompt = f"<s>[INST] Explain the following medical text and its jargons in simple English very concisely: {sent} [/INST]"
+        else:
+            prompt = f"[INST] Next, explain in simple terms for a non-medical audience: {sent} [/INST]"
+
+        input_ids = tokenizer(prompt, return_tensors='pt').input_ids.cuda()
+        output = model.generate(
+            input_ids, temperature=0.6, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=10)
+        input_length = input_ids.size(1)
+        generated_tokens = output[:, input_length:]
+        output_str += tokenizer.decode(
+            generated_tokens[0], skip_special_tokens=True)
+    return output_str
+
+
+def qa_system(query):
+    prompt = f'''<s>[INST]You are a medical chat language model. You will only answer medical-related questions and refuse any and all non-medical questions [/INST]\n\n[INST] Question: {query} [/INST]'''
+    input_ids = tokenizer(prompt, return_tensors='pt').input_ids.cuda()
+    output = model.generate(inputs=input_ids, temperature=0.7,
+                            do_sample=True, top_p=0.95, top_k=40, max_new_tokens=250)
+    input_length = input_ids.size(1)
+    generated_tokens = output[:, input_length:]
+    generated = tokenizer.decode(generated_tokens[0], skip_special_tokens=True)
+    return generated
 
 def custom_canvas(canvas, doc):
     page_width, page_height = letter
